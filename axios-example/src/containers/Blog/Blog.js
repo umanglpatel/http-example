eb878819fcca72ed558d99af36fd13995c6efa56
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-09-12 10:49:03 +0530

    Section 11.12 absolute and relative path. Creating relative path using props.match.url to append path to existing url

--- axios-example/src/containers/Blog/Blog.js
+++ axios-example/src/containers/Blog/Blog.js
@@ -18,7 +18,7 @@ class Blog extends Component {
                         <ul>
                             <li><Link to="/">HOME</Link></li>
                             <li><Link to={{
-                                pathname: '/new-post',
+                                pathname: this.props.match.url + '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
                             }}>New Post</Link></li>
