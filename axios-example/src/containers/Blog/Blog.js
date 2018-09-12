af05a01cccd87f8d92d324faca2e20c0f08d2744
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-09-12 11:45:37 +0530

    Section 11.16 Using swith to only load a single route it two routes matches url because of variable param. Order is important.

--- axios-example/src/containers/Blog/Blog.js
+++ axios-example/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -35,10 +35,12 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>HOME</h1>} />
                 <Route path="/" render={() => <h1>HOME 2</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                {/* Ordering is important here as the below path takes variable arguments after / */}
-                <Route path="/:id" component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    {/* Ordering is important here as the below path takes variable arguments after / */}
+                    <Route path="/:id" component={FullPost} />
+                </Switch>
             </div>
         );
     }
