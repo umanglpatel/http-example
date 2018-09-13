12ba5c26caf08055b9945d47cf7ef5fe63f472e7
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-09-13 11:55:13 +0530

    Section 11.21 Using Redirect component from router-dom to redirect user from one Route to other. one url to other.

--- axios-example/src/containers/Blog/Blog.js
+++ axios-example/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -37,6 +37,8 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
                     <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
+                    {/* <Route path="/" component={Posts} /> */}
                     {/* Ordering is important here as the below path takes variable arguments after / */}
                 </Switch>
             </div>
