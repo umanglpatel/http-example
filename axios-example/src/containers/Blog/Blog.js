3de08250c47c44ecad8da905aaf9030fc7591b22
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-09-13 15:32:13 +0530

    Section 11.26 Adding lazy loading to load components code only when component is going to be loaded.

--- axios-example/src/containers/Blog/Blog.js
+++ axios-example/src/containers/Blog/Blog.js
@@ -4,12 +4,17 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     }
 
     render() {
@@ -39,7 +44,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>HOME</h1>} />
                 <Route path="/" render={() => <h1>HOME 2</h1>} /> */}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not Found</h1>} />
                     {/* <Route render={() => <h1>Not Found</h1>} or use <Route component=404page /> */}
