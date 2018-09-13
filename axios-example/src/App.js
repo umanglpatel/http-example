ab296194fb2773e0fe1ce9b1e9eeb62559cd1076
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-09-13 15:46:13 +0530

    Section 11.27 Routing and the server deployment. Server should forward all the requests to react app to let react app handle error page and etc. if server handles unknown request paths it will not handle 404 from react.

--- axios-example/src/App.js
+++ axios-example/src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename='/my-app'> to add base name to the app this will be prepended to all the routes. default is /
       <BrowserRouter>
         <div className="App">
           <Blog />
