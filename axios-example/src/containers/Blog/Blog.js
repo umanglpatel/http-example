f3360f1b1fb136401e5f81fe3d8fce203b575377
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-09-13 14:46:23 +0530

    Section 11.24 working with guards. Guards guard the un authenticate users from viewing the page. Load component conditionally by checking authentication in state or anything.

--- axios-example/src/containers/Blog/Blog.js
+++ axios-example/src/containers/Blog/Blog.js
@@ -8,6 +8,10 @@ import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
+    state = {
+        auth: false
+    }
+
     render() {
 
         console.log('[Blog] inside render');
@@ -35,7 +39,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>HOME</h1>} />
                 <Route path="/" render={() => <h1>HOME 2</h1>} /> */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                     {/* <Route path="/" component={Posts} /> */}
